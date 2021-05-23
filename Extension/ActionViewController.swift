787adec2ab200a5698939e696344502c21a603a5
Paul Richardson <developer@photographerafoot.com>
2021-05-23 12:13:49 +1000

    CHG: Extract the cgRect from the NSValue

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -53,6 +53,9 @@ class ActionViewController: UIViewController {
 
 	@objc func adjustForKeyboard(notification: Notification) {
 		guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+		let keyboardScreenEndFrame = keyboardValue.cgRectValue
+		
 	}
 
 }
