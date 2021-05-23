22cd0153a2ee3ae4dc42a8365af5c29db6b5c50f
Paul Richardson <developer@photographerafoot.com>
2021-05-23 10:18:00 +1000

    CHG: store title and URL from JavaScript in properties

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -11,6 +11,9 @@ import MobileCoreServices
 class ActionViewController: UIViewController {
 
 	@IBOutlet var script: UITextView!
+
+	var pageTitle = ""
+	var pageURL = ""
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -19,7 +22,11 @@ class ActionViewController: UIViewController {
 				itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
 					guard let itemDictionary = dict as? NSDictionary else { return }
 					guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-					print(javaScriptValues)
+					self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+					self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+					DispatchQueue.main.async {
+						self?.title = self?.pageTitle
+					}
 				}
 			}
 		}
