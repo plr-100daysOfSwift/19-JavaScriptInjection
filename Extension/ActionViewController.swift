44fd4f1a6595c5f2746e176e433ed7b8210fc78b
Paul Richardson <developer@photographerafoot.com>
2021-05-24 12:18:33 +1000

    CHG: convert ActionViewController to a table view controller

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -8,18 +8,27 @@
 import UIKit
 import MobileCoreServices
 
-class ActionViewController: UIViewController {
+class ActionViewController: UITableViewController {
 
-	@IBOutlet var script: UITextView!
+	struct script {
+		var name: String
+		var text: String
+	}
+
+	var scripts = [script]()
 
 	var pageTitle = ""
 	var pageURL = ""
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Scripts", style: .plain, target: self, action: #selector(loadScripts))
+		title = "Scripts"
+
+		scripts.append(script(name: "url", text: "alert(document.URL);"))
+		scripts.append(script(name: "title", text: "alert(document.title);"))
+
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addScript))
 
 		if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
 			if let itemProvider = inputItem.attachments?.first {
@@ -28,52 +37,36 @@ class ActionViewController: UIViewController {
 					guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
 					self?.pageTitle = javaScriptValues["title"] as? String ?? ""
 					self?.pageURL = javaScriptValues["URL"] as? String ?? ""
-					DispatchQueue.main.async {
-						self?.title = self?.pageTitle
-					}
 				}
 			}
 		}
 
-		let notificationCenter = NotificationCenter.default
-		notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
-		notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-
 	}
 
-	@IBAction func done() {
+	// MARK: - Table view data source
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return scripts.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Script", for: indexPath)
+
+		cell.textLabel?.text = scripts[indexPath.row].name
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let script = scripts[indexPath.row].text
 		let item = NSExtensionItem()
-		let argument: NSDictionary = ["customJavaScript": script.text ?? ""]
+		let argument: NSDictionary = ["customJavaScript": script]
 		let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
 		let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
 		item.attachments = [customJavaScript]
 		extensionContext?.completeRequest(returningItems: [item])
-
 	}
 
-	@objc func adjustForKeyboard(notification: Notification) {
-		guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+	@objc func addScript() {
 
-		let keyboardScreenEndFrame = keyboardValue.cgRectValue
-		let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-
-		if notification.name == UIResponder.keyboardWillHideNotification {
-			script.contentInset = .zero
-		} else {
-			script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
-		}
-
-		script.scrollIndicatorInsets = script.contentInset
-
-		let selectedRange = script.selectedRange
-		script.scrollRangeToVisible(selectedRange)
 	}
-
-	@objc func loadScripts() {
-		let storyboard = UIStoryboard(name: "MainInterface", bundle: nil)
-		let vc = storyboard.instantiateViewController(identifier: "ScriptTableViewController")
-		navigationController?.pushViewController(vc, animated: true)
-	}
-
 }
