296c5eee235819a6ba85a0c80918927e97c7446b
Paul Richardson <developer@photographerafoot.com>
2021-05-24 17:05:03 +1000

    CHG: add properties for name and script

--- Extension/DetailViewController.swift
+++ Extension/DetailViewController.swift
@@ -12,21 +12,15 @@ class DetailViewController: UIViewController {
 	@IBOutlet var nameTextField: UITextField!
 	@IBOutlet var scriptTextView: UITextView!
 
+	var name: String?
+	var script: String?
+
 	override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+		nameTextField.text = name ?? "Unknown"
+		scriptTextView.text = script ?? ""
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	}
 
 }
